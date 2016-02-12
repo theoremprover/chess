fd0e1d7621029d74d737b2c4992dcc1c9847bf6e
Robert <no.email@given.de>
2016-02-12 09:10:58 +0100

    No Message

--- chess.hs
+++ chess.hs
@@ -45,20 +45,18 @@ data Move = Move Coors Coors (Maybe Coors) (Maybe PieceType)
 	deriving (Eq,Show)
 
 doMove board (Move from to mb_take mb_promotion) = board // (
-	maybe [] (,Nothing) mb_take :
+	maybe [] ((:[]).(,Nothing)) mb_take ++
 	(from,Nothing) :
 	(to,case mb_promotion of
 		Nothing       -> board!from
 		Just promoted -> Just (my_colour,promoted) where
 			Just (my_colour,_) = board!from ) :
-	case (from,to,board!!from) of
+	case (from,to,board!from) of
 		((5,r),(7,_),(Just (colour,King))) -> [ ((8,r),Nothing),((6,r),Just (colour,Rook)) ]
 		((5,r),(3,_),(Just (colour,King))) -> [ ((1,r),Nothing),((4,r),Just (colour,Rook)) ]
 		_ -> [] )
 
-createBoard position = foldl doMove initialBoard position
-
-moveGenerator position = [ Move from to mb_take mb_promotion |
+moveGenerator moves board = [ Move from to mb_take mb_promotion |
 	(from,Just (colour,piecetype)) <- assocs board,
 	colour == colour_to_move,
 	(to_rel,mb_take_rel,empties_d) <- let
@@ -67,9 +65,10 @@ moveGenerator position = [ Move from to mb_take mb_promotion |
 
 		move_or_take (to_rel,empties) = case addrelcoors from to_rel of
 			Nothing -> Nothing
-			Just to -> case board!!to of
-				Nothing -> (to_rel,Nothing,to_rel)
-				Just (other_colour,_) | other_colour/=colour -> (to_rel,Just to_rel,[])
+			Just to -> case board!to of
+				Nothing -> Just (to_rel,Nothing,to:empties)
+				Just (other_colour,_) | other_colour/=colour -> Just (to_rel,Just to_rel,empties)
+				_ -> Nothing
 
 		can_take_rel on_rel = case addrelcoors from on_rel of
 			Nothing -> False
@@ -77,11 +76,11 @@ moveGenerator position = [ Move from to mb_take mb_promotion |
 
 		in case (piecetype,from) of
 			(Pawn,(f,r)) -> (pawn_dir,Nothing,[pawn_dir]) :
-				if r == pawn_initial_file then [(pawn_dir*2,Nothing,[pawn_dir,pawn_dir*2])] else [] ++
+				(if r == pawn_initial_file then [(pawn_dir*2,Nothing,[pawn_dir,pawn_dir*2])] else []) ++
 				[ (pawn_dir+hor,Just (pawn_dir+hor),[]) | hor <- [east,west], can_take_rel (pawn_dir+hor) ] ++
 				[ (pawn_dir+hor,Just hor,[]) | hor <- [east,west], can_take_rel hor,
-					((Move last_from last_to Nothing Nothing):_) <- reverse position,
-					Just (last_colour,Pawn) <- [board!!last_to], nextColour last_colour == colour,
+					((Move last_from last_to Nothing Nothing):_) <- [reverse moves],
+					Just (last_colour,Pawn) <- [board!last_to], nextColour last_colour == colour,
 					last_from == from+2*pawn_dir+hor, last_to == from+hor ]
 			_ -> catMaybes $ map move_or_take (move_targets piecetype),
 
@@ -90,7 +89,7 @@ moveGenerator position = [ Move from to mb_take mb_promotion |
 		Nothing -> [Nothing]
 		Just take_rel -> case addrelcoors from take_rel of
 			Nothing -> []
-			Just take_on -> if (can_take colour take_on) then [Just take_on] else []
+			Just take_on -> if (can_take colour take_on) then [Just take_on] else [],
 
 	all isNothing $ map (board!) $ catMaybes $ map (addrelcoors from) empties_d,
 	mb_promotion <- case (piecetype,to) of
@@ -100,7 +99,7 @@ moveGenerator position = [ Move from to mb_take mb_promotion |
 
 	where
 
-	can_take mycolour on = maybe False ((== nextColour mycolour).fst) board!!on
+	can_take mycolour on = maybe False ((== nextColour mycolour).fst) (board!on)
 
 	move_targets :: PieceType -> [ (Coors,[Coors]) ]
 	move_targets piecetype = case piecetype of
@@ -118,18 +117,15 @@ moveGenerator position = [ Move from to mb_take mb_promotion |
 		(x,y) | x `elem` [1..8] && y `elem` [1..8] -> Just (x,y)
 		_ -> Nothing
 
-	board = createBoard position
-
 	last_move = if length position == 0 then Nothing else Just (last position)
 
 	(colour_to_move,_):_ = reverse $ zip coloursToMove $ map Just position ++ [Nothing]
 
 putStrConsoleLn s = do
 	putStrLn s
-	appendFile "test.txt" (s++"\n")
+--	appendFile "test.txt" (s++"\n")
 
-showPos position = do
-	let board = createBoard position
+showPos board = do
 	putStrConsoleLn $ "\xbf" ++ replicate 8 '\xc0' ++ "\xc1"
 	forM_ [8,7..1] $ \ rank -> do
 		line <- forM [1..8] $ \ file -> do
@@ -139,11 +135,33 @@ showPos position = do
 		putStrConsoleLn $ [toEnum $ 0xc6 + rank ] ++ line ++ "\xc3"
 	putStrConsoleLn $ "\xc4" ++ map (toEnum.(+0xce)) [1..8] ++ "\xc6"
 
+testBoard = array ((1,1),(8,8)) $ zip [ (f,r) | r <- [8,7..1], f <- [1..8] ] [
+	Nothing,Nothing ,Nothing ,Nothing,b King, Nothing ,Nothing ,b Rook ,
+	Nothing,b Pawn  ,Nothing ,Nothing,Nothing,Nothing ,Nothing ,Nothing,
+	Nothing,Nothing ,Nothing ,Nothing,Nothing,Nothing ,Nothing ,Nothing,
+	w Pawn ,Nothing ,Nothing ,Nothing,Nothing,Nothing ,Nothing ,Nothing,
+	Nothing,Nothing ,Nothing ,Nothing,Nothing,Nothing ,Nothing ,Nothing,
+	Nothing,Nothing ,Nothing ,Nothing,Nothing,Nothing ,Nothing ,Nothing,
+	Nothing,Nothing ,Nothing ,Nothing,Nothing,Nothing ,Nothing ,Nothing,
+	w Rook ,Nothing ,Nothing ,Nothing ,w King , Nothing,Nothing,w Rook ]
+	where
+	w = Just . (White,)
+	b = Just . (Black,)
+
+
 main = do
-	writeFile "test.txt" ""
-	forM_ (moveGenerator initialPosition) $ \ move -> do
+--	writeFile "test.txt" ""
+	step [] testPosition
+
+step moves board = do
+	let board' = createBoard moves board
+	forM_ (moveGenerator moves board') $ \ move -> do
 		putStrConsoleLn "=================================="
 		putStrConsoleLn $ show move
-		showPos [move]
+		showPos board
+		print position
 		s <- getLine
+		case s of
+			"" -> return ()
+			"m" -> step $ position ++ [move]
 		return ()
